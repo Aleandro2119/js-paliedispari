a6662558fc22aab3ecdcaff550706ad5d33901a0
Aleandro2119 <aleandro.basilico@gmail.com>
2022-01-19 17:14:55 +0100

    Changes

--- js/script.js
+++ js/script.js
@@ -4,12 +4,14 @@ console.log('OK JS!');
 
 //Chiedere all’utente di inserire una parola. Creare una funzione per capire se la parola inserita è palindroma
 
+// ! PUNTO 1
 
 const userWord = prompt('Inserisci una parola!');
 
 const result = isPalindroma(userWord);
 console.log(result);
 
+// ! PUNTO 2
 
 function isPalindroma(word) {
     let reverseWord = '';
@@ -24,6 +26,8 @@ function isPalindroma(word) {
         }
     }
 }
+
+
 
 
 
